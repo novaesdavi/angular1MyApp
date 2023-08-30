5118d2f8ea54abca22f496224df8bd30411bf905
Davi Oliveira <novaes.davi1@gmail.com>
2023-08-29 23:03:41 -0300

    até lição 7

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
-
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
@@ -15,11 +15,24 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
     </form>
   </section>
   <section>
-  <app-housing-location></app-housing-location>
+  <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
   </section>
   `,
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  housingLocation: HousingLocation = {
+    id: 1,
+    name: 'Fairfield Apartments',
+    city: 'Fairfield',
+    state: 'IA',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 3,
+    wifi: true,
+    laundry: true
+  };
 
 }
